b81285d5c8dbf1bf0551c37d6629d3f06c0d6f8a
Benjamin Stoneking <benjamin.stoneking@protonmail.com>
2022-07-15 22:10:50 -0600

    Contain audio stream in struct. Next step: set up message channel

--- src/audio.rs
+++ src/audio.rs
@@ -1,6 +1,51 @@
-use cpal::traits::{HostTrait, DeviceTrait};
-use cpal::{Sample, SampleFormat};
+use cpal::traits::{StreamTrait, DeviceTrait};
+use cpal::{Sample, SampleFormat, Host, StreamConfig};
 use rand::Rng;
+
+pub struct AudioStream {
+    audio_stream: cpal::Stream,
+    device: cpal::Device,
+    host: Host,
+    sample_format: SampleFormat,
+    stream_config: StreamConfig,
+}
+
+impl AudioStream {
+    pub fn new(device: cpal::Device, host: Host) -> AudioStream {
+        let mut device_supported_configs = device.supported_output_configs()
+        .expect("Failed to get devices supported output configs");
+        let supported_config = device_supported_configs.next()
+            .expect("No supported config found")
+            .with_max_sample_rate();
+
+        let sample_format = supported_config.sample_format();
+        let config = supported_config.into();
+        let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
+        let stream = match sample_format {
+            SampleFormat::F32 => device.build_output_stream(
+                &config,
+                move |_data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                    // react to stream events and read or write stream data here.
+
+
+                },
+                err_fn),
+            SampleFormat::I16 => device.build_output_stream(&config, output_callback::<i16>, err_fn),
+            SampleFormat::U16 => device.build_output_stream(&config, output_callback::<u16>, err_fn)
+        }.unwrap();
+
+        AudioStream { audio_stream: stream, device, host, sample_format, stream_config: config }
+
+    }
+
+    pub fn play(&self) {
+        self.audio_stream.play().unwrap();
+    }
+
+    pub fn get_sample_format(&self) -> SampleFormat {
+        self.sample_format.clone()
+    }
+}
 
 fn output_callback<T: Sample>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
     for sample in data.iter_mut() {
@@ -16,33 +61,4 @@ fn get_random() -> f32 {
     let mut rng = rand::thread_rng();
     let y: f32 = rng.gen();
     return y
-}
-
-pub fn audio_stream() -> cpal::Stream {
-    let host = cpal::default_host();
-    let device = host.default_output_device()
-        .expect("Failed to get default output device");
-    let mut device_supported_configs = device.supported_output_configs()
-        .expect("Failed to get devices supported output configs");
-    let supported_config = device_supported_configs.next()
-        .expect("No supported config found")
-        .with_max_sample_rate();
-
-    let sample_format = supported_config.sample_format();
-    let config = supported_config.into();
-    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
-    let stream = match sample_format {
-        SampleFormat::F32 => device.build_output_stream(
-            &config,
-            move |_data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-                // react to stream events and read or write stream data here.
-
-
-            },
-            err_fn),
-        SampleFormat::I16 => device.build_output_stream(&config, output_callback::<i16>, err_fn),
-        SampleFormat::U16 => device.build_output_stream(&config, output_callback::<u16>, err_fn)
-    }.unwrap();
-
-    stream
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::io;
-use cpal::traits::StreamTrait;
+use cpal::traits::{HostTrait};
 mod audio;
 mod midi;
 
-fn main() {     
+fn main() {
+    let host = cpal::default_host();
+    let device = host.default_output_device()
+        .expect("Failed to get default output device");
+    
     let _connection = midi::start_midi().unwrap();
-    let audio_stream = audio::audio_stream(); 
-    audio_stream.play().unwrap();
+    let audio_stream = audio::AudioStream::new(device, host);
+
+
+    audio_stream.play();
+
     println!("Press enter to close");
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap(); // wait for next enter key press
